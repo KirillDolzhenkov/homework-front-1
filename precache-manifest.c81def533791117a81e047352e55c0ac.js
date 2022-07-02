c8155a48d1d77a612b5b308ac88e5eaf451eef75
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2022-07-02 22:10:34 +0300

    Updates

---
+++ precache-manifest.c81def533791117a81e047352e55c0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83290eb920f7faf9245f893fc0dee4cc",
+    "url": "/it-incubator-ignat-homeworks/index.html"
+  },
+  {
+    "revision": "b1c5d0446bef703d0622",
+    "url": "/it-incubator-ignat-homeworks/static/css/main.1a50eec0.chunk.css"
+  },
+  {
+    "revision": "a20134222803c5d3366c",
+    "url": "/it-incubator-ignat-homeworks/static/js/2.3bb614ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/it-incubator-ignat-homeworks/static/js/2.3bb614ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1c5d0446bef703d0622",
+    "url": "/it-incubator-ignat-homeworks/static/js/main.f6580613.chunk.js"
+  },
+  {
+    "revision": "c9e417b68838afc2bc48",
+    "url": "/it-incubator-ignat-homeworks/static/js/runtime-main.96718394.js"
+  }
+]);
